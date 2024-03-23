ae753a3a860778b468cb8d85d7dec5ee318538fe
mousesama <mouse0910@gmail.com>
2024-03-23 23:20:00 +0100

    Fixes

--- plugins/english/komga.ts
+++ plugins/english/komga.ts
@@ -1,160 +1,182 @@
-import { fetchApi, fetchFile } from "@libs/fetch";
-import { Plugin } from "@typings/plugin";
-import { FilterTypes, Filters } from "@libs/filterInputs";
-import { load as loadCheerio } from "cheerio";
-import { defaultCover } from "@libs/defaultCover";
-import { NovelStatus } from "@libs/novelStatus";
+import { fetchApi, fetchFile } from '@libs/fetch';
+import { Plugin } from '@typings/plugin';
+import { FilterTypes, Filters } from '@libs/filterInputs';
+import { load as loadCheerio } from 'cheerio';
+import { defaultCover } from '@libs/defaultCover';
+import { NovelStatus } from '@libs/novelStatus';
 // import { isUrlAbsolute } from "@libs/isAbsoluteUrl";
 // import { parseMadaraDate } from "@libs/parseMadaraDate";
-import { AsyncLocalStorage } from "async_hooks";
-import { parseHTML } from "jquery";
+import { AsyncLocalStorage } from 'async_hooks';
+import { parseHTML } from 'jquery';
 
-const username = "mouse0910@gmail.com";
-const password = "Closable-Gong9-Duller";
+const username = 'bW91c2UwOTEwQGdtYWlsLmNvbQoK';
+const password = 'Q2xvc2FibGUtR29uZzktRHVsbGVy';
 
 class Komga implements Plugin.PluginBase {
-    id = "komga";
-    name = "Komga";
-    icon = "src/en/komga/icon.png";
-    site = "https://komga.hibana.me/api/v1";
-    version = "1.0.0";
-    headers = {
-        "Authorization": this.getBasicAuthHeader(),
-    }
-    filters?: Filters | undefined;
+  id = 'komga';
+  name = 'Komga';
+  icon = 'src/en/komga/icon.png';
+  site = 'https://komga.hibana.me/api/v1';
+  version = '1.0.0';
+  headers = {
+    'Authorization': this.getBasicAuthHeader(),
+  };
+  filters?: Filters | undefined;
 
-    getBasicAuthHeader() {
-        const base64Credentials = Buffer.from(`${username}:${password}`).toString('base64');
-        return `Basic ${base64Credentials}`;
-    }
+  getBasicAuthHeader() {
+    const base64Credentials = `${username}:${password}`;
+    return `Basic ${base64Credentials}`;
+  }
 
-    async popularNovels(
-        pageNo: number,
-        {
-            showLatestNovels,
-            filters,
-        }: Plugin.PopularNovelsOptions<typeof this.filters>
-    ): Promise<Plugin.NovelItem[]> {
+  async popularNovels(
+    pageNo: number,
+    {
+      showLatestNovels,
+      filters,
+    }: Plugin.PopularNovelsOptions<typeof this.filters>,
+  ): Promise<Plugin.NovelItem[]> {
+    let link = `${this.site}/books?`;
 
-        let link = `${this.site}/books?`;
-
-        const body = await fetch(link + new URLSearchParams({
-            page: (pageNo - 1).toString(),
-            library_id: "0EZ7QCKACD2EH" // Novels
-            // library_id: "0E99X7XFT6RF1" // Manga
+    const body = await fetch(
+      link +
+        new URLSearchParams({
+          page: (pageNo - 1).toString(),
+          library_id: '0EZ7QCKACD2EH', // Novels
+          // library_id: "0E99X7XFT6RF1" // Manga
         }),
-            { headers: this.headers }
-        ).then((r) => r.json());
+      { headers: this.headers },
+    ).then(r => r.json());
 
-        return body.content.map((book: { name: string; id: string; }): Plugin.NovelItem => ({
-            name: book.name,
-            path: `/books/${book.id}`,
-            cover: `${this.site}/books/${book.id}/thumbnail`,
-        }));
+    return body.content.map(
+      (book: { name: string; id: string }): Plugin.NovelItem => ({
+        name: book.name,
+        path: `/books/${book.id}`,
+        cover: `${this.site}/books/${book.id}/thumbnail`,
+      }),
+    );
+  }
 
-    }
+  async parseNovel(novelPath: string): Promise<Plugin.SourceNovel> {
+    const novel: Plugin.SourceNovel = {
+      path: novelPath,
+      name: 'Untitled',
+    };
 
-    async parseNovel(novelPath: string): Promise<Plugin.SourceNovel> {
-        const novel: Plugin.SourceNovel = {
-            path: novelPath,
-            name: "Untitled",
+    console.log('Parsing novel', novelPath);
+
+    const bookBody = await fetch(this.site + novelPath, {
+      headers: this.headers,
+    }).then(r => r.json());
+    const seriesBody = await fetch(this.site + `/series/${bookBody.seriesId}`, {
+      headers: this.headers,
+    }).then(r => r.json());
+
+    // TODO: get here data from the site and
+    // un-comment and fill-in the relevant fields
+
+    console.log('Fetched data');
+
+    novel.name = bookBody.name;
+    // novel.artist = "";
+    novel.author = bookBody.metadata.authors
+      ? bookBody.metadata.authors
+          .map((author: { name: string }) => author.name)
+          .join(', ')
+      : '';
+    console.log('Bruh');
+    novel.cover = `${this.site}/books/${bookBody.id}/thumbnail`;
+    novel.genres = seriesBody.metadata.genres.join(', ');
+    novel.status = NovelStatus.Completed;
+    novel.summary = seriesBody.metadata.summary;
+
+    console.log('Fetching epub');
+
+    const chaptersBody = await fetch(
+      this.site + `/books/${bookBody.id}/manifest/epub`,
+      { headers: this.headers },
+    ).then(r => r.json());
+
+    console.log(chaptersBody);
+
+    let chapters = chaptersBody.readingOrder.map(
+      (chapter: { href: string }, index: number): Plugin.ChapterItem => {
+        const matchingTocEntry = chaptersBody.toc.find(
+          (tocEntry: { href: string; title: string }) =>
+            tocEntry.href === chapter.href,
+        );
+        return {
+          name: matchingTocEntry ? matchingTocEntry.title : `Chapter ${index}`,
+          path: chapter.href,
+          chapterNumber: index,
         };
+      },
+    );
 
-        console.log("Parsing novel", novelPath)
+    novel.chapters = chapters;
+    console.log(novel);
+    return novel;
+  }
 
-        const bookBody = await fetch(this.site + novelPath, { headers: this.headers }).then((r) => r.json());
-        const seriesBody = await fetch(this.site + `/series/${bookBody.seriesId}`, { headers: this.headers }).then((r) => r.json());
+  async parseChapter(chapterPath: string): Promise<string> {
+    const body = await fetchApi(chapterPath, { headers: this.headers }).then(
+      r => r.text(),
+    );
 
-        // TODO: get here data from the site and
-        // un-comment and fill-in the relevant fields
+    console.log('Parsing chapter', chapterPath);
 
-        console.log("Fetched data")
+    const $ = loadCheerio(body);
 
-        novel.name = bookBody.name;
-        // novel.artist = "";
-        novel.author = bookBody.metadata.authors.map((author: { name: string; }) => author.name).join(", ");
-        console.log("Bruh")
-        novel.cover = `${this.site}/books/${bookBody.id}/thumbnail`;
-        novel.genres = seriesBody.metadata.genres.join(", ");
-        novel.status = NovelStatus.Completed;
-        novel.summary = seriesBody.metadata.summary;
+    const baseUrl = chapterPath.substring(
+      0,
+      chapterPath.indexOf('/OEBPS/') + '/OEBPS/'.length,
+    );
 
-        console.log("Fetching epub")
+    $('img').each(function () {
+      const img = $(this);
+      let src = img.attr('src');
 
-        const chaptersBody = await fetch(this.site + `/books/${bookBody.id}/manifest/epub`, { headers: this.headers }).then((r) => r.json());
+      if (src && src.startsWith('../')) {
+        src = src.replace('../', baseUrl);
+        img.attr('src', src);
+      }
+    });
 
-        console.log(chaptersBody)
+    // parse chapter text here
+    const chapterText = $.html();
+    return chapterText;
+  }
 
-        let chapters = chaptersBody.readingOrder.map((chapter: { href: string; }, index: number): Plugin.ChapterItem => {
+  async searchNovels(
+    searchTerm: string,
+    pageNo: number,
+  ): Promise<Plugin.NovelItem[]> {
+    let link = `${this.site}/books?`;
 
-            const matchingTocEntry = chaptersBody.toc.find((tocEntry: { href: string; title: string; }) => tocEntry.href === chapter.href);
-            return {
-                name: matchingTocEntry ? matchingTocEntry.title : `Chapter ${index}`,
-                path: chapter.href,
-                chapterNumber: index
-            }
-        });
-
-        
-        novel.chapters = chapters;
-        console.log(novel)
-        return novel;
-    }
-
-    async parseChapter(chapterPath: string): Promise<string> {
-        const body = await fetchApi(chapterPath, { headers: this.headers }).then((r) => r.text());
-
-        console.log("Parsing chapter", chapterPath)
-
-        const $ = loadCheerio(body);
-
-        const baseUrl = chapterPath.substring(0, chapterPath.indexOf("/OEBPS/") + "/OEBPS/".length);
-
-        $('img').each(function() {
-            const img = $(this);
-            let src = img.attr('src');
-
-            if (src && src.startsWith("../")) {
-                src = src.replace("../", baseUrl)
-                img.attr('src', src);
-            }
-        });
-        
-        // parse chapter text here
-        const chapterText = $.html();
-        return chapterText;
-    }
-
-    async searchNovels(
-        searchTerm: string,
-        pageNo: number
-    ): Promise<Plugin.NovelItem[]> {
-
-        let link = `${this.site}/books?`
-
-        const body = await fetch(link + new URLSearchParams({
-            page: (pageNo - 1).toString(),
-            library_id: "0EZ7QCKACD2EH",
-            search: searchTerm
+    const body = await fetch(
+      link +
+        new URLSearchParams({
+          page: (pageNo - 1).toString(),
+          library_id: '0EZ7QCKACD2EH',
+          search: searchTerm,
         }),
-            { headers: this.headers }
-        ).then((r) => r.json());
+      { headers: this.headers },
+    ).then(r => r.json());
 
-        return body.content.map((book: { name: string; id: string; }): Plugin.NovelItem => ({
-            name: book.name,
-            path: `/books/${book.id}`,
-            cover: `${this.site}/books/${book.id}/thumbnail`,
-        }));
-    }
+    return body.content.map(
+      (book: { name: string; id: string }): Plugin.NovelItem => ({
+        name: book.name,
+        path: `/books/${book.id}`,
+        cover: `${this.site}/books/${book.id}/thumbnail`,
+      }),
+    );
+  }
 
-    async fetchImage(url: string): Promise<string | undefined> {
-        // if your plugin has images and they won't load
-        // this is the function to fiddle with
+  async fetchImage(url: string): Promise<string | undefined> {
+    // if your plugin has images and they won't load
+    // this is the function to fiddle with
 
-        return await fetchFile(url, { headers: this.headers });
-    }
-
+    return await fetchFile(url, { headers: this.headers });
+  }
 }
-
 
 export default new Komga();
